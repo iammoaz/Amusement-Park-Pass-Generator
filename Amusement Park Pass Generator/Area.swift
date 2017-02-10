d317633992047aa6803670aabd7d0bed54414978
Muhammad Moaz <m-moaz@hotmail.com>
2017-02-10 21:43:44 +0300

    Model access entitlements

---
+++ Amusement Park Pass Generator/Area.swift
@@ -0,0 +1,17 @@
+//
+//  Area.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Muhammad Moaz on 2/10/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import Foundation
+
+enum Area {
+    case amusement
+    case kitchen
+    case rideControl
+    case maintenance
+    case office
+}

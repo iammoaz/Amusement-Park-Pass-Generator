d64f6e863fcf569d05d28fc1f1b0e4af181bbe74
Muhammad Moaz <m-moaz@hotmail.com>
2017-02-12 15:57:07 +0300

    Create date helper methods

---
+++ Amusement Park Pass Generator/Date.swift
@@ -0,0 +1,29 @@
+//
+//  Date.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Muhammad Moaz on 2/11/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import Foundation
+
+// Calculates age in seconds from the number
+extension Int {
+    var inSeconds: TimeInterval {
+        return TimeInterval(60 * 60 * 24 * 365.2422 * Double(self))
+    }
+}
+
+// Constructs a date using the components specified
+extension Date {
+    static func getDateFrom(year: Int, month: Int, Day: Int) -> Date? {
+        let calendar = Calendar.current
+        var components = DateComponents()
+        components.year = year
+        components.month = month
+        components.day = Day
+        
+        return calendar.date(from: components)
+    }
+}

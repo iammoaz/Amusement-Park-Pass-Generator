7ec4f4908f38379917291488a3a5afcefb0623eb
Muhammad Moaz <moaz.aamir@gmail.com>
2017-02-19 12:57:58 +0300

    Add global theme object to handle UI

---
+++ Amusement Park Pass Generator/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Muhammad Moaz on 2/17/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+    static var buttonBorderColor: UIColor {
+        return UIColor(red: 0.34, green: 0.58, blue: 0.56, alpha: 1.00)
+    }
+}
